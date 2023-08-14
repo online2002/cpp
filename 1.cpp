28ee4c67cf84759b63ea41a24192559b84d4ea7a
online2002 <125761595+online2002@users.noreply.github.com>
2023-08-14 10:34:36 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,13 +2,11 @@
 using namespace std;
 int main(){
     //declaring pair 
-
     //datatype couuld be anything also containers...
     pair<int,string> p[3];
     p[0]={1,"a"};
     p[1]={2,"b"};
     p[2]={3,"c"};
-
     //in built swap function
     //swap(p[0],p[2]);
     for(int i = 0;i<3;i++){
