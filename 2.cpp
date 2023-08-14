053e2664c8cddf5b22607a3edad1facdac413e05
online2002 <onlinereinvention@gmail.com>
2023-08-14 10:33:02 +0530

    adding 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> v;
+    for(int i=1;i<11;i++){
+        v.push_back(i);
+    }
+    for(int i =0 ; i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+}
