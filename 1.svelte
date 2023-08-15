a305286e506ec25c24c00225bd7609ad8aa42b0c
online2002 <onlinereinvention@gmail.com>
2023-08-15 16:00:03 +0530

    adding svelte file

---
+++ 1.svelte
@@ -0,0 +1,12 @@
+<script>
+    let bool = false;
+    function change(){
+        bool = !bool;
+    }
+</script>
+{#if bool}
+    <button on:click={change}></button>
+{/if}
+{#if !bool}
+    <button on:click={change}></button>
+{/if}
